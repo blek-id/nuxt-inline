aeb6d5c4c4ca880cd01d613d68a93c117ce5fcd5
kelvin-junilson <kelvin.junilson@bridestory.com>
2019-11-18 18:15:57 +0700

    Init

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div class="container">
+        <div class="inline">Text</div>
+        <div class="inline">Text</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StartPage"
+};
+</script>
+
+<style scoped>
+.inline {
+    display: inline-block;
+    width: 50%;
+}
+</style>
